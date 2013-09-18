97f0f35f38a2c3b1836794b0aae16bbfffa4fe4f
kazuya sato <sato@fact-real.com>
2013-09-18 17:37:02 +0900

    Add peep.carousel js and css.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 peep.carousel
-=============
+---
+
+**1.設置**
+
+Zeptoとpeep.carouselのJS、CSSファイルを読み込む。
+
+```
+<link rel="stylesheet" type="text/css" href="peep.carousel/peep.carousel.css">
+<script type="text/javascript" src="http://zeptojs.com/zepto.min.js"></script>
+<script type="text/javascript" src="peep.carousel/peep.carousel.js"></script>
+```
+
+**2.HTMLコーディング**
+
+カルーセルを`div`で囲み、クラスに`peep-carousel`、idに任意のIDを振る。
+
+```
+<div id="peep1" class="peep-carousel">
+    <ul>
+        <li><img src="img/sample01.jpg" width="94" height="94"></li>
+        <li><img src="img/sample02.jpg" width="94" height="94"></li>
+        <li><img src="img/sample03.jpg" width="94" height="94"></li>
+        <li><img src="img/sample03.jpg" width="94" height="94"></li>
+        .
+        .
+        .
+    </ul>
+</div>
+```
+
+**3.プラグイン呼び込み**
+
+任意のIDを指定してpeep.carouselを呼び込む。
+
+```
+<script type="text/javascript">
+$(function() {
+    $('#peep1').peepCarousel();
+});
+</script>
+```
---
+++ peep.carousel/peep.carousel.js
@@ -0,0 +1,147 @@
+﻿/*
+ * peep carousel v0.1
+ * Copyright 2013 Kazuya Sato
+ */
+
+if (typeof Object.create !== 'function') {
+  Object.create = function(obj) {
+    function F() {};
+      F.prototype = obj;
+      return new F();
+  };
+}
+
+(function($, window, document) {
+  var Carousel = {
+    init: function(options, el) {
+      var base = this;
+      base.options = $.extend({}, $.fn.peepCarousel.options, options);
+      var elem = el;
+      var $elem = $(el);
+      base.$elem = $elem;
+
+      base.startUp();
+    },
+
+    startUp: function() {
+      var base = this;
+      base.carouselId = base.$elem.attr('id');
+      base.carouselUl = base.$elem.find('ul');
+      base.carouselLi = base.carouselUl.find('li');
+      base.carouselLiFirst = base.carouselLi.first();
+      base.carouselLiLast = base.carouselLi.last();
+      base.imageSize = base.carouselLi.find('img').size();
+      base.count = base.minCount = 1;
+      base.maxCount = parseInt(base.imageSize / base.options.displayImageNum);
+
+      base.$elem.css('width', 'auto');
+      base.carouselUl.height(base.carouselLi.height());
+
+      base.buildCarousel();
+      base.buildNavigation();
+
+      $(window).resize(function() {
+        base.buildCarousel();
+      });
+    },
+
+    buildCarousel: function() {
+      var base = this;
+      var windowWidth = $(window).width();
+      var paddingPxPercent = base.options.paddingPercent * windowWidth;
+      var paddingPx = windowWidth * paddingPxPercent;
+      var carouselDisplayWidth = windowWidth - paddingPx * 2;           
+      var carouselLiWidth = carouselDisplayWidth / base.options.displayImageNum;     
+      var carouselUlWidth = carouselLiWidth * base.imageSize + paddingPx * 2;
+
+      base.translateXWidth = carouselLiWidth * base.options.displayImageNum;          
+      base.carouselUl.width(carouselUlWidth);
+      base.carouselLi.width(carouselLiWidth);
+      base.carouselLiFirst.css('padding-left', paddingPx);
+      base.carouselLiLast.css('padding-right', paddingPx);
+
+      base.translateNaviView(base.count - 1);
+    },
+
+    buildNavigation: function() {
+      var base = this;
+      var naviTemplate = '<ul class="carouselNavi"><li class="prev">' + base.options.navigationText[0]+ '</li><li class="next">' + base.options.navigationText[1]+ '</li></ul><ul class="carouselNaviView">';
+
+      for (var i = 0; i <= base.maxCount; i++) {
+        if (i == 0) {
+          naviTemplate = naviTemplate + '<li class="active"></li>';
+        } else {
+          naviTemplate = naviTemplate + '<li></li>';
+        }
+      }
+      naviTemplate = naviTemplate + '</ul'>
+      base.$elem.append(naviTemplate);
+      base.naviViewLi = base.$elem.children('ul.carouselNaviView').find('li');
+
+      $(document).on('tap click', '#' + base.carouselId + ' li.prev', function() {
+        base.prev();
+      });
+
+      $(document).on('tap click', '#' + base.carouselId + ' li.next', function() {
+        base.next();
+      });
+    },
+
+    prev: function() {
+      var base = this;
+
+      if (base.count > base.minCount) {
+        base.count--;
+        var count = base.count - 1;
+        base.translateNaviView(count);
+        base.initNaviView(count);
+      }
+
+      return false;
+    },
+
+    next: function() {
+      var base = this;
+
+      if (base.count <= base.maxCount) {
+        base.initNaviView();
+        base.translateNaviView();
+        base.count++;
+      }
+
+      return false;
+    },
+
+    initNaviView: function(count) {
+      var base = this;
+      if (typeof count === "undefined") count = base.count;
+
+      base.naviViewLi.removeClass('active');
+      base.naviViewLi.eq(count).addClass('active');
+    },
+
+    translateNaviView: function(count) {
+      var base = this;
+      if (typeof count === "undefined") count = base.count;
+
+      var translateWidth = - base.translateXWidth * count;
+      base.carouselUl.css({
+        '-webkit-transform' : 'translate3d(' + translateWidth + 'px,0,0)',
+        '-webkit-transition': '-webkit-transform 400ms cubic-bezier(0,0,0.25,1)'
+      });
+    }
+  };
+
+  $.fn.peepCarousel = function(options) {
+    return this.each(function() {
+      var carousel = Object.create(Carousel);
+      carousel.init(options, this);
+    });
+  };
+
+  $.fn.peepCarousel.options = {
+    displayImageNum : 3,
+    paddingPercent  : 0.00015,
+    navigationText  : ['前へ', '次へ']
+  };
+})(Zepto, window, document);
